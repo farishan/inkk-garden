ecb935852f20ec950dd2907613cd6683583aa3e9
Faris Hanugraha <farishanugraha@gmail.com>
2020-05-03 16:23:35 +0700

    Add plants views

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
+import Plants from '../views/Plants.vue';
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home,
+  },
+  {
+    path: '/plants',
+    name: 'Plants',
+    component: Plants,
   },
   {
     path: '/about',
---
+++ src/views/Plants.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="raw">
+    <h3>Raw Database</h3>
+    <pre>
+      {{ JSON.stringify($store.state.plants, null, 2) }}
+    </pre>
+  </div>
+</template>
+
+<style scoped>
+  .raw {
+    text-align: left;
+    max-width: 300px;
+    max-height: 600px;
+    overflow-y: auto;
+  }
+</style>
