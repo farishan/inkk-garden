3220631da7b0b8f53b041c4e419825a645525327
Faris Hanugraha <farishanugraha@gmail.com>
2020-05-03 21:55:10 +0700

    Add bar component

---
+++ src/components/Bar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div
+    class="absolute w-full transition duration-200"
+    :class="classes"
+    :style="style"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    customStyle: Object,
+    width: Number,
+    classes: String,
+  },
+  data() {
+    return {
+      style: {
+        transformOriginX: 0,
+        transform: `scaleX(${this.width})`,
+        height: '3px',
+        ...this.customStyle,
+      },
+    };
+  },
+};
+</script>
--- src/components/Potslot.vue
+++ src/components/Potslot.vue
@@ -1,24 +1,78 @@
 <template>
-  <div class="border">
-    <div
-      class="plant"
-      v-if="data === null"
-    >
+  <div class="border relative">
+
+    <div class="plant" v-if="data === null">
       null
     </div>
+
     <div
       v-if="data !== null"
       class="plant"
       :style="{backgroundImage: `url('/images/plants/${data.image}')`}"
     >
       {{ data.name }}
+      <bar
+        :width="progressWidth"
+        :classes="'bg-green-400'"
+        :customStyle="{bottom: '3px'}"
+      />
+      <bar
+        :width="waterWidth"
+        :classes="'bg-blue-400 bottom-0'"
+      />
+      <bar
+        :width="cookieWidth"
+        :classes="'bg-black top-0'"
+        v-show="data.ready"
+      />
+      <bar
+        :width="healthWidth"
+        :classes="'bg-red-400 top-0'"
+        v-show="data.dry"
+      />
     </div>
   </div>
 </template>
 
 <script>
+import Bar from './Bar.vue';
+
 export default {
-  props: ['data'],
+  components: {
+    Bar,
+  },
+  props: {
+    data: Object,
+  },
+  computed: {
+    progressWidth() {
+      const param = this.data.progress;
+      const param1 = 100;
+      return this.calc(param, param1);
+    },
+    waterWidth() {
+      const param = this.data.water;
+      const param1 = this.data.waterCapacity;
+      return this.calc(param, param1);
+    },
+    cookieWidth() {
+      const param = this.data.cookieHealth;
+      const param1 = this.data.defaultCookieHealth;
+      return this.calc(param, param1);
+    },
+    healthWidth() {
+      const param = this.data.health;
+      const param1 = this.data.defaultHealth;
+      return this.calc(param, param1);
+    },
+  },
+  methods: {
+    // Calculate bar width
+    calc(param, param1) {
+      const calc = param ? param / param1 : 0;
+      return calc > 0 ? calc : 0;
+    },
+  },
 };
 </script>
 
