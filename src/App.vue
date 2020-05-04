10cbfa567c5308be443bb639f41f7b266a69d0c5
Faris Hanugraha <farishanugraha@gmail.com>
2020-05-05 01:11:57 +0700

    Display player's cookies

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <Time />
+    <p class="absolute bottom-0">player cookies: {{ $store.state.player.cookies }}</p>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/plants">Plants</router-link> |
