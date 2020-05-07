87549c5d384cca767046166c10c7a6db48d20eaa
Faris Hanugraha <farishanugraha@gmail.com>
2020-05-07 15:16:30 +0700

    Trivial store edit

--- src/store/modules/time.store.js
+++ src/store/modules/time.store.js
@@ -7,71 +7,68 @@
  * Faris Han, 2020
  */
 
-const state = () => ({
-  initialSpeed: 2, // seconds
-  speed: 2, // seconds
-  on: false,
-  days: 0,
-  periods: 0,
-  fast: false,
-});
+const timeModule = {
+  namespaced: true,
+  state: () => ({
+    initialSpeed: 2, // seconds
+    speed: 2, // seconds
+    on: false,
+    days: 0,
+    periods: 0,
+    fast: false,
+  }),
+  getters: {
+    period: (state) => (state.periods % 2 === 0 ? 'day' : 'night'),
+  },
+  actions: {
+    start({ commit, dispatch }) {
+      commit('switch', true);
+      dispatch('startLoop');
+    },
+    startLoop({ state, commit, dispatch }) {
+      let last = 0; // timestamp of the last render() call
 
-const getters = {
-  period: (state) => (state.periods % 2 === 0 ? 'day' : 'night'),
-};
+      function loop(now) {
+        // Execute every [state.speed] seconds
+        if (!last || now - last >= state.speed * 1000) {
+          last = now;
+          commit('addPeriod');
 
-const actions = {
-  start({ state, commit, dispatch }) {
-    commit('switch', true);
-    let last = 0; // timestamp of the last render() call
+          if (state.periods % 2 === 0) {
+            commit('addDay');
+            dispatch('update', null, { root: true });
+          }
+        }
 
-    function loop(now) {
-      // Execute every [state.speed] seconds
-      if (!last || now - last >= state.speed * 1000) {
-        last = now;
-        commit('addPeriod');
-
-        if (state.periods % 2 === 0) {
-          commit('addDay');
-          dispatch('update', null, { root: true });
+        if (state.on) {
+          requestAnimationFrame(loop);
         }
       }
 
-      if (state.on) {
-        requestAnimationFrame(loop);
-      }
-    }
-
-    loop(last);
+      loop(last);
+    },
+  },
+  mutations: {
+    switch(state, value) {
+      state.on = value;
+    },
+    fast(state) {
+      state.on = false;
+      state.fast = true;
+      state.speed /= 4;
+    },
+    normal(state) {
+      state.on = false;
+      state.fast = false;
+      state.speed = state.initialSpeed;
+    },
+    addPeriod(state) {
+      state.periods += 1;
+    },
+    addDay(state) {
+      state.days = Math.floor(state.periods / 2);
+    },
   },
 };
 
-const mutations = {
-  switch(state, value) {
-    state.on = value;
-  },
-  fast(state) {
-    state.on = false;
-    state.fast = true;
-    state.speed /= 4;
-  },
-  normal(state) {
-    state.on = false;
-    state.fast = false;
-    state.speed = state.initialSpeed;
-  },
-  addPeriod(state) {
-    state.periods += 1;
-  },
-  addDay(state) {
-    state.days = Math.floor(state.periods / 2);
-  },
-};
-
-export default {
-  namespaced: true,
-  state,
-  getters,
-  actions,
-  mutations,
-};
+export default timeModule;
